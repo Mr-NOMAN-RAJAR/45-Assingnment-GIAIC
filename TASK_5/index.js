f6e3012f7e1cc66608d3c012e03f508d6acb13e8
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 01:19:19 +0500

    Delete TASK_5 directory

--- TASK_5/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Assaignment NO 5
-// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
-Object.defineProperty(exports, "__esModule", { value: true });
-let famous_person = "ALLAMA IQBAL ";
-let quote = "Life is nothing but regret It is not what it is, it has not happened, it has not been found, it has not been";
-let massage = `${famous_person} once said," ${quote}"`;
-console.log(massage);
