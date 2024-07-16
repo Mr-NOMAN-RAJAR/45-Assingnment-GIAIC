965dcca72d80bf1298f909935ff6934273601798
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 01:46:05 +0500

    Delete TASK-13 directory

--- TASk-13/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// 13. Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
-// to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
-let transportMode = ["CAR", "cd70", "Buss", "Moterbike"];
-for (let i = 0; i < transportMode.length; i++) {
-    console.log(`such as “I would like to own a  ${transportMode[i]}`);
-}
