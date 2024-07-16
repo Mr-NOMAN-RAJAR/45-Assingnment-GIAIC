4924dfdbcc9bcc1bec25635eb99082e4589e775f
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 02:01:24 +0500

    Add files via upload

---
+++ TASK-22/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let ErrorArry = ["NOMAN", 20, "Karachi"];
+console.log(ErrorArry[3]);
+console.log(ErrorArry[2]);
---
+++ TASK-22/index.ts
@@ -0,0 +1,4 @@
+"use strict";
+let ErrorArry = ["NOMAN", 20, "Karachi"];
+console.log(ErrorArry[3]);
+console.log(ErrorArry[2]);
