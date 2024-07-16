96da5a24783c110b3b9d3ab5255a0c7be9a3add4
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 02:06:03 +0500

    Add files via upload

---
+++ TASK-26/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let allien_color = "Green";
+if (allien_color === "Green") {
+    console.log("Congratulation! You just earned 5 Points");
+}
+else {
+    console.log("Congratulation! You just earned 10 Points");
+}
+//2nd part
+allien_color = "red";
+if (allien_color === "Green") {
+    console.log("Congratulation! You just earned 5 Points");
+}
+else {
+    console.log("Congratulation! You just earned 10 Points");
+}
---
+++ TASK-26/index.ts
@@ -0,0 +1,15 @@
+let allien_color = "Green";
+if (allien_color === "Green") {
+    console.log("Congratulation! You just earned 5 Points");
+}
+else {
+    console.log("Congratulation! You just earned 10 Points");
+}
+//2nd part
+allien_color = "red";
+if (allien_color === "Green") {
+    console.log("Congratulation! You just earned 5 Points");
+}
+else {
+    console.log("Congratulation! You just earned 10 Points");
+}
