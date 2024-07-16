844eb5c3eca63f96e97b3f455e4fc8abf7ef7ffc
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 01:31:07 +0500

    Add files via upload

---
+++ TASK-4/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Assignment NO:4
+Object.defineProperty(exports, "__esModule", { value: true });
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+var Quote = "Oh God! You are the owner of this world or Creator of our life.";
+let author = "SHAH ABDUL LATIF BHITTAI";
+console.log(`${author} once said, "${Quote}" `);
