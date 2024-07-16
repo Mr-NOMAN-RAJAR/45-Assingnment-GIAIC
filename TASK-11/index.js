3d5fb8e5443885653d227d2121868faf0a5ba7c0
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 01:34:55 +0500

    Add files via upload

---
+++ TASK-11/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// 11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time
+Object.defineProperty(exports, "__esModule", { value: true });
+let arry = ["noman", "khan", "ahmed", 'arbab'];
+// console.log(arry[0]);
+// console.log(arry[1]);
+// console.log(arry[2]);
+// console.log(arry[3]);
+for (let i = 0; i < arry.length; i++) {
+    console.log(arry[i]);
+}
---
+++ TASK-11/index.ts
@@ -0,0 +1,11 @@
+// 11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time
+
+
+let arry: string[]=["noman", "khan", "ahmed" ,'arbab']
+
+
+for (let i = 0; i < arry.length; i++) {
+    console.log(arry[i]);
+    
+    
+}
