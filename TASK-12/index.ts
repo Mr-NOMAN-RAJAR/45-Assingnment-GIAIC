d9a29095006ab899ee7f457fb824ec5f52c2e24c
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 01:36:02 +0500

    Add files via upload

---
+++ TASK-12/index.ts
@@ -0,0 +1,13 @@
+// 12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message
+// should be the same, but each message should be personalized with the person’s name.
+
+
+let arry: string[]=["noman", "bilal", "ahmed" ,'arbab']
+
+
+arry.forEach(element => {
+    console.log(`Hello ${element}! How are you? `);
+    
+});
+
+
