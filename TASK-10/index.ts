13d3f3a2d2f63151352c6fe8d27f622eaa70c9da
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 01:34:21 +0500

    Add files via upload

---
+++ TASK-10/index.ts
@@ -0,0 +1,13 @@
+// 10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write
+// because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence
+// describing what the program does.
+
+// simple calculation in litral template
+// difine two number and print the risult.
+
+let num1: number = 4;
+let num2: number = 6;
+console.log(`${num1 * num2}`)
+
+
+
