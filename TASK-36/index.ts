b93273d23bea30fee8db9f6a51c450cc324f033f
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 19:14:11 +0500

    Add files via upload

---
+++ TASK-36/index.ts
@@ -0,0 +1,6 @@
+function make_shirt(size:string, text:string){
+    console.log(`Creating a ${size} with the message ${text}`);
+}
+make_shirt("Large", "Hello");
+make_shirt("Medium", "Hello");
+make_shirt("Small", "Hello");
