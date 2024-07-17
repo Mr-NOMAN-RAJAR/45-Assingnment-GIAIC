c6c9e32926dd6588ffcfcc2a36f4131a50f78776
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 19:02:37 +0500

    Add files via upload

---
+++ TASK-28/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 3;
+if (age < 2) {
+    console.log("The Person is a baby.");
+}
+else if (age >= 2 && age < 4) {
+    console.log("The Person is a toddier. ");
+}
+else if (age >= 4 && age < 13) {
+    console.log("The Person is a Kid. ");
+}
+else if (age >= 13 && age < 20) {
+    console.log("The Person is a Teenager. ");
+}
+else if (age >= 20 && age < 65) {
+    console.log("The Person is an Adult. ");
+}
+else {
+    console.log("The Person is an elder.");
+}
