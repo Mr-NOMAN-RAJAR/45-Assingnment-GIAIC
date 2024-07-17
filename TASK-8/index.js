dd524e463b1676ffe1289548c908b179ba9921ac
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 19:42:18 +0500

    Delete TASK-8 directory

--- TASK-8/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-//   7. Number Eight: Write addition, subtraction, multiplication, and division operations thatS each result in the number 8. Be sure to enclose your operations in print statements to see the results. 
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log(5 + 3);
-console.log(12 - 4);
-console.log(4 * 2);
-console.log(16 / 2);
