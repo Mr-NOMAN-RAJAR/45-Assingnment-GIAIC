3f39689786f36a574aaa76594f6f2f4e644b8b69
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 19:16:57 +0500

    Add files via upload

---
+++ TASK-38/index.ts
@@ -0,0 +1,6 @@
+function describe_city(city:string, country = "Default Country.") {
+    console.log(`${city} is in ${country}`);
+}
+describe_city("Karachi", "Pakistan");
+describe_city("Dehli", "India");
+describe_city("Dubai");
