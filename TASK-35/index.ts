10035ed11082c16065800756185e73f3298bbd39
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 19:12:45 +0500

    Add files via upload

---
+++ TASK-35/index.ts
@@ -0,0 +1,5 @@
+let Animals = ["dog", "cat", "rabbit"];
+for (let Animal of Animals) {
+    console.log(`A ${Animal} would make a great pet.`);
+}
+console.log("Any of these animals would make a great pet!");
