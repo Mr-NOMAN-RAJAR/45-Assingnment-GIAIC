919420cf963379c07c910e7de4319f79669d73fe
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 19:22:25 +0500

    Add files via upload

---
+++ TASK-41/index.js
@@ -0,0 +1,8 @@
+let magicians = ["Harry Potter", "Derren Brown", "Dynamo", "Shin Lim"];
+function show_magicians(magicians) {
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+show_magicians(magicians);
+export {};
---
+++ TASK-41/index.ts
@@ -0,0 +1,10 @@
+
+let magicians: string[] = ["Harry Potter", "Derren Brown", "Dynamo", "Shin Lim"];
+
+function show_magicians(magicians: string[]): void {
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+
+show_magicians(magicians);
