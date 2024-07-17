86e050be35083ff4beac275426141ecf125b1f2e
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 19:15:37 +0500

    Add files via upload

---
+++ TASK-37/idex.js
@@ -0,0 +1,7 @@
+function make_shirt(size = "Large", text = "I love TypeScript.") {
+    console.log(`Creating a ${size} with the message ${text}`);
+}
+make_shirt();
+make_shirt("Medium");
+make_shirt("Small", "I love JavaScript.");
+export {};
