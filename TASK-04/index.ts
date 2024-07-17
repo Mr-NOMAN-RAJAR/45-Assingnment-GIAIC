5e515d02b4b92288350a485a0292d781ece19630
NOMAN <164003451+Mr-NOMAN-RAJAR@users.noreply.github.com>
2024-07-17 19:35:39 +0500

    Add files via upload

---
+++ TASK-04/index.ts
@@ -0,0 +1,8 @@
+// Assignment NO:4
+
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+const Quote: string = "Oh God! You are the owner of this world or Creator of our life."
+let author: string = "SHAH ABDUL LATIF BHITTAI"
+
+console.log(`${author} once said, "${Quote}" `)
